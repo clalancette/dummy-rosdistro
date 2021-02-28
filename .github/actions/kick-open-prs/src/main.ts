a174097681a747297e950ccca90f1b5cbcfb50ae
Chris Lalancette <clalancette@openrobotics.org>
2021-02-28 22:12:04 +0000

    Debugging on kick-open-prs.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- .github/actions/kick-open-prs/src/main.ts
+++ .github/actions/kick-open-prs/src/main.ts
@@ -9,75 +9,33 @@ async function run() {
     try {
         const token = core.getInput("repo-token", { required: true });
 
-        const pullRequest = github.context.payload.pull_request;
-        if (!pullRequest) {
-            console.log("Could not get pull request number from context, exiting");
-            return;
-        }
-        const prNumber = pullRequest.number;
-
         const client = new github.GitHub(token);
 
-        console.log(`fetching changed files for pr #${prNumber}`);
+        const prList: number[] = await getOpenPRs(client);
 
-        core.debug(`fetching changed files for pr #${prNumber}`);
-        const changedFiles: string[] = await getChangedFiles(client, prNumber);
-        console.log(`changed files: ${changedFiles}`);
-
-        console.log(`About to read sync-freeze.yml`);
-        // This reads the sync-freeze.yaml value for whatever branch/tag was specified
-        // in the workflow file.  Since that should (usually) be the 'master' branch,
-        // that means that you cannot both freeze/unfreeze a distribution and make changes to
-        // the distribution in the same PR.
-        const sync_data = await fs.readFile("sync-freeze.yaml", "utf8");
-        const sync_freeze = yaml.safeLoad(sync_data);
-
-        const frozen_distros: Map<string, boolean> = new Map();
-        for (const distro in sync_freeze["distributions"]) {
-            frozen_distros.set(distro, sync_freeze["distributions"][distro]["freeze"]);
-        }
-
-        for (const filename of changedFiles) {
-            console.log(`filename is ${filename}`);
-            const modified_distro: string = path.dirname(filename);
-            console.log(`Modified distro is ${modified_distro}`);
-            if (frozen_distros.has(modified_distro) && frozen_distros.get(modified_distro)) {
-                console.log("In freeze!");
-                // FIXME: if we are in freeze, we can do one or more of the following:
-                // - Add a comment to the PR saying "ROS distribution 'foo' is in sync freeze, holding..."
-                // - Add a label to the PR with "sync-freeze"
-                // - Fail the CI check
-                //const repo = github.context.repo;
-                //client.issues.createComment({...repo, body: "hello", issue_number: prNumber});
-                core.setFailed(`ROS distribution ${modified_distro} is in freeze`);
-                return;
-            }
-        }
     } catch (error) {
         core.error(error);
         core.setFailed(error.message);
     }
 }
 
-async function getChangedFiles(
-    client: github.GitHub,
-    prNumber: number
-): Promise<string[]> {
-    const listFilesOptions = client.pulls.listFiles.endpoint.merge({
+async function getOpenPRs(client: github.GitHub): Promise<Array<number>> {
+    // FIXME: .endpoint.merge here, followed by paginate like above?
+    const prListOptions = await client.pulls.list.endpoint.merge({
+        state: 'open',
         owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: prNumber
-    });
+        repo: github.context.repo.repo
+    })
 
-    const listFilesResponse = await client.paginate(listFilesOptions);
-    const changedFiles = listFilesResponse.map(f => f.filename);
+    const prList = await client.paginate(prListOptions);
 
-    core.debug("found changed files:");
-    for (const file of changedFiles) {
-        core.debug("  " + file);
+    var prNums = new Array<number>();
+    for (const pr of prList) {
+        console.log(`PR: ${pr}`);
+        //prNums.push(pr.number);
     }
 
-    return changedFiles;
+    return prNums;
 }
 
 run();
